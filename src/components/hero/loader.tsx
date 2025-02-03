a96730b3671b9813eb283aa0e71310b4e3193c3f
AreebaSadiq23 <areebasadiq41@gmail.com>
2025-02-03 11:04:33 +0500

    update loader

--- src/components/hero/loader.tsx
+++ src/components/hero/loader.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { Cloud } from 'lucide-react';
+'use client'
+import React, { useEffect, useState } from "react";
 
 function Loader() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null; 
+
   return (
-    <div className='flex justify-center items-center h-screen bg-gradient-to-br from-gray-100 to-gray-300'>
-      <div className='relative'>
-        {/* Outer spinning border */}
-        <div className='animate-spin rounded-full h-24 w-24 border-t-4 border-b-4 border-yellow-500 border-opacity-70'></div>
-        
-        {/* Inner slower spinning border */}
-        <div className='absolute top-0 left-0 h-24 w-24 rounded-full border-t-4 border-b-4 border-transparent animate-spin-slow border-yellow-300 border-opacity-50'></div>
-        
-        {/* Center icon */}
-        <div className='absolute inset-0 flex items-center justify-center text-black'>
-          <Cloud size={40} className="opacity-90 animate-pulse" />
+    <div className="flex items-center justify-center h-screen bg-gradient-to-br from-gray-100 to-gray-300">
+      <div className="flex flex-col gap-4 w-full items-center justify-center">
+        {/* Outer Loader */}
+        <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
+          {/* Inner Loader */}
+          <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"></div>
         </div>
       </div>
     </div>
